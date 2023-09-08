77621d8f36e4d93ea88e1f4afac419ece51315a2
Filip Śliwiński <275498@student.pwr.edu.pl>
2023-09-08 19:36:20 +0200

    Started writing discrete functions utils

---
+++ General/DiscreteFunctions.cs
@@ -0,0 +1,77 @@
+﻿using MathNet.Numerics;
+using MathNet.Numerics.LinearAlgebra;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quantum_Mechanics.General
+{
+    public static class DiscreteFunctions
+    {
+        public static double Integrate(Vector<double> x, Vector<double> y, double[] bounds)
+        {
+            var result = 0d;
+
+            var n = y.Count;
+            var dx = (x[1] - x[0]) / (n - 1);
+            var a = (int)((bounds[0] - x[0]) / dx);
+            var b = (int)((bounds[1] - x[0]) / dx);
+
+            for (int i = a; i <= b; ++i)
+            {
+                if (i < 0 || i >= n)
+                    continue;
+
+                result += y[i] * dx;
+            }
+
+            return result;
+        }
+
+        public static Complex32 IntegrateComplex(Vector<double> x, Vector<Complex32> y, double[] bounds)
+        {
+            var result = Complex32.Zero;
+
+            var n = y.Count;
+            var dx = (float)(x[1] - x[0]) / (n - 1);
+            var a = (int)((bounds[0] - x[0]) / dx);
+            var b = (int)((bounds[1] - x[0]) / dx);
+
+            for (int i = a; i <= b; ++i)
+            {
+                if (i < 0 || i >= n)
+                    continue;
+
+                result += y[i] * dx;
+            }
+
+            return result;
+        }
+
+        public static Vector<Complex32> Fourier(Vector<double> domain, Vector<Complex32> samples, int n)
+        {
+            var result = CreateVector.Sparse<Complex32>(n);
+            var dx = (float)((domain[1] - domain[0]) / (n - 1));
+
+            for (int i = 0; i < n; ++i)
+            {
+                var sample = Complex32.Zero;
+                var k = (float)(domain[0] + i * dx);
+
+                for (int j = 0; j < n; ++j)
+                {
+                    var x = (float)(domain[0] + j * dx);
+
+                    sample += samples[j] * 1f / MathF.Sqrt(2 * MathF.PI) * Complex32.Exp(-Complex32.ImaginaryOne * k * x);
+                }
+
+                result[i] = sample;
+            }
+
+            return result;
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using MathNet.Numerics;
 using MathNet.Numerics.Differentiation;
+using MathNet.Numerics.IntegralTransforms;
 using MathNet.Numerics.Integration;
 using MathNet.Numerics.LinearAlgebra;
 using MathNet.Numerics.Optimization;
@@ -13,10 +14,42 @@ using System.Runtime.Versioning;
 
 Control.UseNativeMKL();
 Console.OutputEncoding = System.Text.Encoding.UTF8;
-
+/*
 var R = CreateVector.SparseOfArray(new double[] { 0, 5 });
 var psi = new QuantumSystem1D(QuantumConstants.Me, 1, "0", R.ToArray());
 
 var p = psi.WaveFunction.GetMomentumSpaceValues();
 
-VisualizationTool.Plot(p, R.ToArray(), 400);
+VisualizationTool.Plot(p, R.ToArray(), 400);*/
+
+var n = 1000;
+
+var R = CreateVector.SparseOfArray(new double[] { -2.5, 2.5 });
+var x = CreateVector.Sparse<double>(n);
+var y = CreateVector.Sparse<Complex32>(n);
+
+var dx = (R[1] - R[0]) / (n - 1);
+
+for (int i = 0; i < n; ++i)
+{
+    x[i] = R[0] + i * dx;
+    y[i] = Complex32.Sqrt(0.4f) * Complex32.Sin(MathF.PI * (float)x[i] / 5f);
+}
+
+var F = DiscreteFunctions.Fourier(R, y, n);
+var f = CreateVector.Sparse<double>(n);
+var N = 0f;
+
+for (int i = 0; i < n; ++i)
+{
+    f[i] = F[i].MagnitudeSquared;
+    N += (float)f[i];
+}
+f *= 1 / N;
+
+var plot = new Plot();
+plot.SetAxisLimits(R[0], R[1], f.Min(), f.Max());
+plot.AddSignalXY(x.ToArray(), f.ToArray());
+plot.SaveFig("plot.png");
+
+Process.Start("explorer.exe", "plot.png");
