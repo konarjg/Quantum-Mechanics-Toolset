398bbca1e93cc34fed6e4d3fc64f969572ceb77b
Filip Śliwiński <275498@student.pwr.edu.pl>
2023-09-11 18:21:17 +0200

    Started creating 2D cartesian system

---
+++ Quantum Mechanics/QuantumConstants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quantum_Mechanics.Quantum_Mechanics
+{
+    public static class QuantumConstants
+    {
+        //Reduced Planck's Constant H = h / 2pi
+        public static double H = 1.054571817;
+        //Mass of a single electron
+        public static double Me = 0.0091093837;
+        //Kinetic energy of a single electron
+        public static double Te = -H * H / (2 * Me);
+    }
+}
