5a514d8129265341b51499d664382c4e69837dd2
Filip Śliwiński <275498@student.pwr.edu.pl>
2023-09-13 09:29:08 +0200

    A little more progress

--- DE Solver/Interpolator.cs
+++ DE Solver/Interpolator.cs
@@ -23,16 +23,11 @@ namespace Quantum_Mechanics.DE_Solver
             return new DiscreteFunction(u);
         }
 
-        public static DiscreteFunction2D Bicubic(Vector<double> x, Vector<double> y, Vector<double> u)
+        public static DiscreteFunction2D Bicubic(Vector<double> x, Vector<double> y, Matrix<double> u)
         {
             var f = new Func<double, double, double>((x0, y0) =>
             {
-                var x1 = new double[u.Count];
-
-                for (int i = 0; i < u.Count; ++i)
-                    x1[i] = Interpolate.CubicSpline(x, u).Interpolate(x0);
-
-                return Interpolate.CubicSpline(y, x1).Interpolate(y0);
+                
             });
 
             return new DiscreteFunction2D(f);
--- Program.cs
+++ Program.cs
@@ -3,23 +3,23 @@ using Quantum_Mechanics.DE_Solver;
 using ScottPlot;
 
 var n = (int)Math.Sqrt(1000);
-var dx = 2e9 / (n - 1);
-var dy = 2e9 / (n - 1);
+var dx = 2 * Math.PI / (n - 1);
+var dy = 2 * Math.PI / (n - 1);
 var x = CreateVector.Sparse<double>(n);
 var y = CreateVector.Sparse<double>(n);
-var u = CreateVector.Sparse<double>(n * n);
+var u = CreateMatrix.Sparse<double>(n, n);
 
 for (int i = 0; i < n; ++i)
 {
     for (int j = 0; j < n; ++j)
     {
-        x[i] = -1e9 + i * dx;
-        y[j] = -1e9 + j * dy;
+        x[i] = 0 + i * dx;
+        y[j] = 0 + j * dy;
 
-        u[n * i + j] = Math.Sin(x[i]) + Math.Sin(y[i]);
+        u[i, j] = 0.08 * Math.Pow(Math.Sin(Math.PI * x[i] / 5) * Math.Sin(Math.PI * y[i] / 5), 2);    
     }
 }
 
 var f = Interpolator.Bicubic(x, y, u);
 
-Console.WriteLine(f.Evaluate(Math.PI / 4, Math.PI / 4));
+Console.WriteLine(f.Integrate(0, 5, 0, 5));
