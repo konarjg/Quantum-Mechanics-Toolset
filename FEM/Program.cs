36c088039cf72ed3ed61ceb5f366e7e88b8a2d98
Filip Śliwiński <275498@student.pwr.edu.pl>
2023-10-29 17:35:26 +0100

    Testing FDM performance for polar separated PDEs

--- FEM/Program.cs
+++ FEM/Program.cs
@@ -2,4 +2,4 @@
 using MathNet.Numerics.LinearAlgebra;
 using System.Numerics;
 
-SpectralSolver.SolveODEBessel(new double[] { 0, 5 }, 1, 3, 100);
+SpectralSolver.SolveODEBessel(new double[] { 0, 5 }, 2, 0, 100);
--- FEM/SpectralSolver.cs
+++ FEM/SpectralSolver.cs
@@ -13,6 +13,7 @@ using Complex = System.Numerics.Complex;
 using ScottPlot;
 using System.Diagnostics;
 using Accord.Extensions.BinaryTree;
+using System.Net.Http.Headers;
 
 namespace FEM
 {
@@ -64,16 +65,27 @@ namespace FEM
 
         public static void SolveODEBessel(double[] domain, int n, int l, int gridPoints)
         {
-            var dx = (domain[1] - domain[0]) / (gridPoints - 1);
-            var dy = 2 * Math.PI / (gridPoints - 1);
-            var d = 2 * domain[1] / (gridPoints - 1);
-            var x = new double[gridPoints];
-            var y = new double[gridPoints];
+            var dr = (domain[1] - domain[0]) / (gridPoints - 1);
+            var dtheta = 2 * Math.PI / (gridPoints - 1);
+            var dx = 2 * domain[1] / (gridPoints - 1);
+
+            var x = new double[gridPoints * gridPoints];
+            var y = new double[gridPoints * gridPoints];
+            var r = new double[gridPoints];
+            var theta = new double[gridPoints];
+
+            //Kocham misiaczka <3
 
             for (int i = 0; i < gridPoints; ++i)
             {
-                x[i] = domain[0] + i * dx;
-                y[i] = -Math.PI + i * dy;
+                r[i] = domain[0] + i * dr;
+
+                for (int j = 0; j < gridPoints; ++j)
+                {
+                    theta[j] = j * dtheta;
+                    x[gridPoints * i + j] = r[i] * Math.Cos(theta[j]);
+                    y[gridPoints * i + j] = r[i] * Math.Sin(theta[j]);
+                }
             }
 
             var H = CreateMatrix.Sparse<Complex>(gridPoints, gridPoints);
@@ -81,12 +93,12 @@ namespace FEM
             for (int i = 0; i < gridPoints; ++i)
             {
                 if (i + 1 < gridPoints)
-                    H[i, i + 1] = -0.978 / (dx * dx) - 0.978 / (2 * dx) * 1 / (x[i] + 0.1);
+                    H[i, i + 1] = -0.978 / (dr * dr) - 0.978 / (2 * dr) * 1 / (r[i] + 0.1);
 
                 if (i - 1 >= 0)
-                    H[i, i - 1] = -0.978 / (dx * dx) + 0.978 / (2 * dx) * 1 / (x[i] + 0.1);
+                    H[i, i - 1] = -0.978 / (dr * dr) + 0.978 / (2 * dr) * 1 / (r[i] + 0.1);
 
-                H[i, i] = 2 * 0.978 / (dx * dx) - 1 / (x[i] * x[i] + 0.1);
+                H[i, i] = 2 * 0.978 / (dr * dr) + (l * l) / (r[i] * r[i] + 0.1) - 1 / (r[i] + 0.1);
             }
 
             var evd = H.Evd();
@@ -94,65 +106,45 @@ namespace FEM
             var E = evd.EigenValues;
             var U = evd.EigenVectors;
 
-            for (int i = 0; i < E.Count; ++i)
-            {
-                if (E[i].Real < 0)
-                    ++n;
-            }
-
             var L = CreateVector.Sparse<Complex>(gridPoints);
             var R = CreateVector.Sparse<Complex>(gridPoints);
-            var Nr = 0d;
-            var Nl = 0d;
 
             for (int i = 0; i < gridPoints; ++i)
             {
-                L[i] = Complex.Exp(-Complex.ImaginaryOne * l * y[i]);
+                L[i] = Complex.Exp(Complex.ImaginaryOne * l * theta[i]);
                 R[i] = U.Column(n)[i];
-
-                Nr += R[i].MagnitudeSquared() * x[i] * dx;
-                Nl += L[i].MagnitudeSquared() * dy;
             }
 
-            var fl_real = Interpolate.CubicSpline(y, L.Real() / Nl);
-            var fl_imag = Interpolate.CubicSpline(y, L.Imag() / Nl);
-            var fr = Interpolate.CubicSplineRobust(x, R / Nr);
+            var psi = R.OuterProduct(L);
+            var N = 0d;
 
-            var frl = new Func<double, double, double>((x, y) =>
+            for (int i = 0; i < gridPoints; ++i)
             {
-                var r = Math.Sqrt(x * x + y * y);
-                var l = Math.Atan2(y, x);
+                for (int j = 0; j < gridPoints; ++j)
+                    N += psi[i, j].MagnitudeSquared() * r[i] * dr * dtheta;
+            }
 
-                return fr.Interpolate(r) * fl.Interpolate(l);
-            });
-
+            psi /= Math.Sqrt(N);
             var u = CreateMatrix.Sparse<double>(gridPoints, gridPoints);
 
             for (int i = 0; i < gridPoints; ++i)
             {
                 for (int j = 0; j < gridPoints; ++j)
-                {
-                    var x0 = -domain[1] + i * d;
-                    var y0 = -domain[1] + j * d;
-
-                    var f = frl(x0, y0);
-
-                    if (!f.IsFinite())
-                        u[i, j] = 0;
-                    else
-                        u[i, j] = f;
-                }
+                    u[i, j] = psi[i, j].MagnitudeSquared();
             }
 
+            u = u.Transpose();
             Console.WriteLine("E{0} = {1}", n + 1, E[n]);
 
             var plot = new Plot();
-            plot.SetAxisLimits(-domain[1], domain[1], -domain[1], domain[1]);
+            plot.SetAxisLimits(0, 2 * Math.PI, domain[0], domain[1]);
             var map = plot.AddHeatmap(u.ToArray());
-            map.CellWidth = d;
-            map.CellHeight = d;
-            map.OffsetX = -domain[1];
-            map.OffsetY = -domain[1];
+            map.CellWidth = dtheta;
+            map.CellHeight = dr;
+            map.OffsetX = 0;
+            map.OffsetY = domain[0];
+            map.UseParallel = true;
+            map.Opacity = 1;
 
             plot.AddColorbar(map);
             plot.SaveFig("plot.png");
